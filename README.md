88e47c7f66ca0fc56ed9e8f2829b96da4eb796a9
Luis <luisbarrios0110@gmail.com>
2024-11-07 10:03:42 -0500

    Add part 3: 3.2 (Implement route  http://localhost:3001/info  for pphonebook info

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ con diferentes tecnologias y herramientas.
 ## Part 3 - A 
 
 - 3.1 Backend Phonebook (paso 1: Levantando un servidor para la API de los datos importantes de las personas que se registran en la web)
+- 3.2 Backend Phonebook (paso 2: Implementando en la API la direccion  http://localhost:3001/info para resumen basico la consulta Get)
 
 
 
--- index.js
+++ index.js
@@ -23,24 +23,51 @@ const persons = [
       id: 4,
       name: "Mary Poppendieck", 
       number: "39-23-6423122"
+    },
+    { 
+      id: 4,
+      name: "Mary Cuie", 
+      number: "83-43-127023"
     }
 ]
-//Funciones y presentaciones
+//Funciones y presentacion
 app.get('/',(req,res)=>{
-  res.send(`
+  const presentation =`
     <h1>Rest Persons</h1>
     <p>Get information about persons via a RESTful API</p>
     <br> 
-    <div> use.. <br>
-     <a href="http://localhost:3001/api/persons">/api/persons</a>      for view all persons <br>
+    <div>use.. <br>
+     <a href="http://localhost:3001/api/persons">/api/persons</a>      for view all persons </br>
+     <a href="http://localhost:3001/info">/info</a> to view total persons in phonebook and more info </br>
     </div>
-    `);
+    `;
+    res.send(presentation);
 })
 
+app.get('/info',(req,res)=>{
+  const totalpersons = persons.length
+  const currentDate = new Date;
+
+  const information = `
+  <div>
+    <p>Phonebook has info for ${totalpersons} people</p>
+    <p>${currentDate}</p>
+
+    <br><br><br>
+    <a href="/" > ← Back </a>
+  </div>
+  `;
+  res.send(information)
+})
+
+//API REST
 app.get('/api/persons',(req,res)=>{
   res.json(persons)
 })
 
+app.post('/api/persons',(req,res)=>{
+  res.send('Hola')
+})
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT} for exit pres CTRL + C` );
