9222b81e3fba151571e3b1a1d103bfabe62669c6
Luis <luisbarrios0110@gmail.com>
2024-11-09 10:39:47 -0500

    Add Part 3: 3.8 (Show with new token in morgan the POST request body {name,number}

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 3001;
 app.use(express.json());
 const morgan = require('morgan');
-app.use(morgan('tiny'));
+morgan.token('body', (req,res)=>{return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms, :body'));
 
 let persons = [
     { 
