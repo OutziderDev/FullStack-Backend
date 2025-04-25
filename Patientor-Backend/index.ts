1fd890d5a04d80a694998e8bd693d5ae20808c9c
Luis <luisbarrios0110@gmail.com>
2025-04-24 20:08:55 -0500

    Add Part 9: 9.9 Patientor Backend (add cors to response the ping from the frontend)

--- Patientor-Backend/index.ts
+++ Patientor-Backend/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
-const PORT = 3003;
+const PORT = 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
